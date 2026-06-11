f80367edd3e2e46950be6181cea3cc8fd890e53a
afighufran <leticiahd132@gmail.com>
2026-06-11 16:37:02 +0700

    nambahin prevent destroy

--- env-ubuntu/main.tf
+++ env-ubuntu/main.tf
@@ -21,53 +21,57 @@ module "ubuntu_24_server" {
   vm_gateway   = var.vm_gateway
 }
 
-module "db_server" {
-  source = "../modules/vsphere-vm"
+# module "db_server" {
+#   source = "../modules/vsphere-vm"
 
-  vsphere_user       = var.vsphere_user
-  vsphere_password   = var.vsphere_password
-  vsphere_server     = var.vsphere_server
-  vsphere_datacenter = var.vsphere_datacenter
-  vsphere_cluster    = var.vsphere_cluster
-  vsphere_datastore  = var.vsphere_datastore
-  vsphere_network    = var.vsphere_network
-  vsphere_template   = var.vsphere_template
+#   vsphere_user       = var.vsphere_user
+#   vsphere_password   = var.vsphere_password
+#   vsphere_server     = var.vsphere_server
+#   vsphere_datacenter = var.vsphere_datacenter
+#   vsphere_cluster    = var.vsphere_cluster
+#   vsphere_datastore  = var.vsphere_datastore
+#   vsphere_network    = var.vsphere_network
+#   vsphere_template   = var.vsphere_template
 
-  vm_count       = 3
-  vm_name_prefix = "db-server"
-  vm_cpu         = 4
-  vm_ram         = 4096
+#   vm_count       = 3
+#   vm_name_prefix = "db-server"
+#   vm_cpu         = 4
+#   vm_ram         = 4096
 
-  vm_ip_prefix = "172.16.9"
-  vm_ip_start  = 190
-  vm_netmask   = var.vm_netmask
-  vm_gateway   = var.vm_gateway
-}
+#   vm_ip_prefix = "172.16.9"
+#   vm_ip_start  = 190
+#   vm_netmask   = var.vm_netmask
+#   vm_gateway   = var.vm_gateway
+# }
 
-module "app_server" {
-  source = "../modules/vsphere-vm"
+# module "app_server" {
+#   source = "../modules/vsphere-vm"
 
-  vsphere_user       = var.vsphere_user
-  vsphere_password   = var.vsphere_password
-  vsphere_server     = var.vsphere_server
-  vsphere_datacenter = var.vsphere_datacenter
-  vsphere_cluster    = var.vsphere_cluster
-  vsphere_datastore  = var.vsphere_datastore
-  vsphere_network    = var.vsphere_network
-  vsphere_template   = var.vsphere_template
+#   vsphere_user       = var.vsphere_user
+#   vsphere_password   = var.vsphere_password
+#   vsphere_server     = var.vsphere_server
+#   vsphere_datacenter = var.vsphere_datacenter
+#   vsphere_cluster    = var.vsphere_cluster
+#   vsphere_datastore  = var.vsphere_datastore
+#   vsphere_network    = var.vsphere_network
+#   vsphere_template   = var.vsphere_template
 
-  vm_count       = 3
-  vm_name_prefix = "app-server"
-  vm_cpu         = 2
-  vm_ram         = 2048
+#   vm_count       = 3
+#   vm_name_prefix = "app-server"
+#   vm_cpu         = 2
+#   vm_ram         = 2048
 
-  vm_ip_prefix = "172.16.9"
-  vm_ip_start  = 195
-  vm_netmask   = var.vm_netmask
-  vm_gateway   = var.vm_gateway
-}
+#   vm_ip_prefix = "172.16.9"
+#   vm_ip_start  = 195
+#   vm_netmask   = var.vm_netmask
+#   vm_gateway   = var.vm_gateway
+# }
 
 moved {
   from = vsphere_virtual_machine.vm_belajar
   to   = module.ubuntu_24_server.vsphere_virtual_machine.vm_belajar
+}
+
+lifecycle {
+  prevent_destroy = true
 }
